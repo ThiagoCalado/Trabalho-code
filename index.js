6a2e3139803077fbffbe2166b9eb1580eb2cd7aa
ThiagoCalado <115361544+ThiagoCalado@users.noreply.github.com>
2023-03-24 11:22:30 -0300

    Trabalho dia: 24/03/2023

---
+++ index.js
@@ -0,0 +1,14 @@
+const nome = 'Thiago';
+const sobreNome = 'Calado';
+console.log(nome, sobreNome)
+let idade = 23;
+console.log(idade)
+peso = 65;
+console.log(peso)
+altura = 1.77
+console.log(altura)
+imc = peso / (altura *altura);
+console.log(imc);
+let anoAtual = 2023;
+anoNascimento = anoAtual - idade;
+console.log(anoNascimento);
