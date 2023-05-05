b11cc366301bcbb8610762d74a45f052bbd6517f
ThiagoCalado <115361544+ThiagoCalado@users.noreply.github.com>
2023-05-05 11:18:59 -0300

    05/05/2023

---
+++ App IMC/assets/js/main.js
@@ -0,0 +1,35 @@
+function pegarInfos() {
+    const form = document.querySelector(".form");
+
+    const resultado = document.querySelector(".resultado")
+
+    function recebeEvento(evento) {
+        evento.preventDefault();
+        const altura = form.querySelector('#altura');
+        const peso = form.querySelector('#peso');
+        const imc = peso / (altura * altura);
+
+        if (imc < 24) {
+            console.log("peso esta normal");
+        }
+        else if (imc < 17) {
+            console.log("peso esta baixo do normal");
+        }
+        else if (imc > 25) {
+            console.log('esta com sobrepeso');
+        }
+        else if (imc > 29) {
+            console.log('esta com Obesos: Classe I');
+        }
+        else if (imc > 34) {
+            console.log('esta com Obesos: Classe II');
+        }
+        else if (imc > 40) {
+            console.log('esta com Obesos: Classe III');
+        }
+        
+    }
+
+    form.addEventListener('submit', recebeEvento);
+}
+pegarInfos()
