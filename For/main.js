2a6b9c59dbbf34909e87da08f091f279486e5e50
ThiagoCalado <115361544+ThiagoCalado@users.noreply.github.com>
2023-05-22 11:05:00 -0300

    22/05/2023

---
+++ For/main.js
@@ -0,0 +1,50 @@
+//const num = [20,30,40,50,70,100,200,300];
+//console.log(num[1]);
+//const [pri, seg, terc] = num;
+// console.log(seg);
+//const [pri, seg, ...resto] = num;
+//console.log(seg, resto);
+
+//array dentro de array
+
+// const arrayNum = [[1,2,3], [4,5,6], [7,8,9]];
+// const [lista1, lista2, lista3] = arraNum;
+// cosole.log(lista2[2]);
+
+// atribuição vai desestruturação em objetos
+
+// const objeto = {
+//     nome: 'Lulu',
+//     idade: 69,
+//     endereco:{
+//         rua: 'Av americas',
+//         nu: 1400,
+//         cep:'57962-666'
+//     }
+// }
+
+// const {nome, idade, endereco: local} = objeto;
+
+// console.log(local);
+// console.log(idade);
+// const {nome, idade, ...resto} = objeto;
+// console.log(resto);
+
+//For
+// console.log('linha 0')
+// console.log('linha 1')
+// console.log('linha 2')
+// console.log('linha 3')
+// console.log('linha 4')
+// console.log('linha 5')
+// console.log('linha 6')
+
+// for(let i=0; i < 7; i++){
+//     console.log(`linha ${i}`);
+// }
+
+// const cars = ['ferrari', 'masserati', 'mustang', 'land rover'];
+
+// for(let i=0; i < cars.length; i++){
+//     console.log(cars[i]);
+// }
