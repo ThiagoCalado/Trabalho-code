b0381907fbe810b8f383bfd561aa3c55aa4b5aa1
ThiagoCalado <115361544+ThiagoCalado@users.noreply.github.com>
2023-05-29 11:03:31 -0300

    29/05/2023

---
+++ curso/main.js
@@ -0,0 +1,35 @@
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+// const frutas = ['maça', 'banana', 'abacaxi', 'abacati'];
+// for (let kay in frutas){
+//     console.log(frutas[key]);
+// }
+
+// const pessoa = {
+//     nome: 'Vini',
+//     idade: 27,
+//     raca: 'humano',
+//     class: 'barbaro'
+// }
+
+// for(let abc in pessoa){
+//     console.log(pessoa[abc]);
+// }
+
+// const ler = 'biruleibe do leibe';
+
+// for (let i = 0; i < ler.length; i++){
+//     console.log(ler[i])
+// }
+
+// for(let i in ler){
+//     console.log(ler[i]);
+// }
+
+// for(let i of ler){
+//     console.log(i);
+// }
---
+++ curso/while.js
@@ -0,0 +1,8 @@
+let controle = 0
+while(controle <= 10){
+    console.log(controle);
+    console.log('estou no loop');
+
+    controle++;
+}
+console.log('sai do loop');
