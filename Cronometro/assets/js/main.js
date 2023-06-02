d2eb1933d028010a2011eda10d87b5b764259573
ThiagoCalado <115361544+ThiagoCalado@users.noreply.github.com>
2023-06-02 11:14:59 -0300

    02/06/2023

---
+++ Cronometro/assets/js/main.js
@@ -0,0 +1,36 @@
+function contagemSegundo(){
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR',{
+        hour12:false,
+        timeZone: 'GMT'
+    })
+};
+
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar= document.querySelector('.zerar');
+
+let segundos = 0;
+let timer;
+
+function inicarContagem(){
+    timer = setInterval(() => {
+        segundos ++;
+        relogio.innerHTML = contagemSegundo(segundos);
+    },1000);
+}
+
+iniciar.addEventListener('click', (e)=>{
+    inicarContagem();
+})
+
+pausar.addEventListener('click', (e)=>{
+    clearInterval(timer);
+})
+
+zerar.addEventListener('click', (e)=>{
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0
+})
