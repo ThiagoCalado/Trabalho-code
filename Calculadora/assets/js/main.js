2c278338e34d71fd51ac575c05fcc6221b8ebacb
ThiagoCalado <115361544+ThiagoCalado@users.noreply.github.com>
2023-06-16 12:04:15 -0300

    16/06/2023

---
+++ Calculadora/assets/js/main.js
@@ -0,0 +1,65 @@
+function criaCalculadora(){
+    return{
+        display: document.querySelector('.display'),
+
+        iniciar(){
+            this.clickBotoes();
+        },
+
+        clearDisplay(){
+            this.display.value = '';
+        },
+
+        apagaUm(){
+            this.display.value = this.display.value.slice(0, -1);
+        },
+
+        realizarConta(){
+            let conta = this.display.value;
+            
+            try {
+                conta = eval(conta);
+
+                if(!conta){
+                    alert('Conta Inválida');
+                    return;
+                }
+                this.display.value = String(conta)
+            } catch (error) {
+                alert('Conta Inválida');
+                return;
+            }
+        },
+
+        clickBotoes(){
+            document.addEventListener('click', (e) =>{
+                const el = e.target;
+                if (el.classList.contains('btn-num')){
+                    this.mostraNoDisplay(el.innerText);
+                }
+                
+                if(el.classList.contains('btn-clear')){
+                    this.clearDisplay();
+                }
+                
+                if(el.classList.contains('btn-del')){
+                    this.apagaUm();
+                }
+
+                if(el.classList.contains('btn-eq')){
+                    this.realizarConta();
+                }
+
+                this.display.focus();
+            })
+        },
+
+        mostraNoDisplay(valor){
+            this.display.value += valor;
+        }
+    }
+
+}
+
+const calculadora = criaCalculadora();
+calculadora.iniciar();
